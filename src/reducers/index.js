1e39baee086ff10f12aaae0c604673b2c3c1ae70
Deepak245 <amrith.dwadasi@gmail.com>
2021-05-23 01:13:05 +0530

    Commit with change in Index file at class 239

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+
+
+
+const songsReducer =()=>{
+    return [
+        {title: 'No Scrubs' , duration: '4.05'},
+        {title: 'Macarena', duration: '2:38'},
+        {title: 'All Star', duration:'3:15'},
+        {title: 'I want it That Way', duration :'1.45'}
+    ];
+};
+
+
+const selectedSongReducer =(selectedSong=null, action)=>{
+    if(action.type==='SONG_SELECTED'){
+        return action.payload;
+    }
+
+        return selectedSong;
+
+}
