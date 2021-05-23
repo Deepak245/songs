d023a0442101802c750c02a1121b1c4831a8835e
Deepak245 <amrith.dwadasi@gmail.com>
2021-05-23 16:34:04 +0530

    Completed Class 241

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -5,9 +5,16 @@ import {connect} from 'react-redux';
 
 class SongList extends Component{
     render(){
+        console.log(this.props);
         return <div>SongList</div>;
     }
 
 }
 
-export default connect()(SongList);
+const mapStateToProps=(state)=>{
+    console.log(state);
+    return state;
+
+}
+
+export default connect(mapStateToProps)(SongList);
